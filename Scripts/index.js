f00f578883b5a051eec53cb04b4763064be66408
Silassentinel <53788992+Silassentinel@users.noreply.github.com>
2023-08-16 18:02:56 +0200

    Update index.js

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,1 +1,16 @@
-init
+export default function run () {
+
+const element = document.querySelector('#keepMeVisible');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+        console.log("in view")
+    } else {
+      console.log("out of view")
+    }
+  });
+});
+window.addEventListener('scroll', function() {
+    observer.observe(element);
+});
+}
