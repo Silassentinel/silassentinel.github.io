e9099245d853bdc0f72f3612f02586670c01b1f3
Silassentinel <53788992+Silassentinel@users.noreply.github.com>
2023-08-26 12:08:15 +0200

    updates & jscodings

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,16 +1,79 @@
-export default function run () {
+// GLOABLS VERY BAD FOR NOW
 
-const element = document.querySelector('#keepMeVisible');
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-        console.log("in view")
-    } else {
-      console.log("out of view")
-    }
+const username = "silassentinel";
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------
+
+function run()
+{
+
+  const element = document.querySelector('#keepMeVisible');
+  const observer = new IntersectionObserver((entries) =>
+  {
+    entries.forEach((entry) =>
+    {
+      if (entry.isIntersecting)
+      {
+        console.log("in view");
+      } else
+      {
+        console.log("out of view");
+      }
+    });
   });
-});
-window.addEventListener('scroll', function() {
+  window.addEventListener('scroll', function ()
+  {
     observer.observe(element);
-});
+  });
 }
+
+async function getAllRepos(username)
+{
+  try
+  {
+    const response = await fetch(`https://api.github.com/users/${username}/repos?per_page=100`);
+    const repos = await response.json();
+    return repos;
+  } catch (error)
+  {
+    console.error("Error fetching repositories:", error);
+    return [];
+  }
+}
+
+function jsonToUnorderedList(jsonArray) {
+  let ul = document.createElement("ul");
+
+  jsonArray.forEach(item => {
+    let li = document.createElement("li");
+    li.textContent = item.name;
+    ul.appendChild(li);
+  });
+
+  return ul;
+}
+
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------
+
+if (document.readyState === "loading")
+{
+  try
+  {
+    const repos =  (async () => {
+      return await getAllRepos(username);
+    })();
+    console.log(repos);
+  }
+  catch (error)
+  {
+    console.error("Error fetching repositories:", error);
+  }
+}
+else if (document.readyState === "interactive") { }
+else if (document.readyState === "complete")
+{
+  run();
+  jsonToUnorderedList(repos)
+}
+else { }
